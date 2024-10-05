236e2fe5bfe885ea1c28f759e94c57edc4265905
Shahied <shahied@Shahieds-MacBook-Pro.local>
2024-10-05 17:50:13 +0800

    added logic for table ranking and structured data for chart

--- src/app/teams/team-view/table-ranking/table-ranking.component.ts
+++ src/app/teams/team-view/table-ranking/table-ranking.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { GameWeekService } from '../gameweek/gameweek-service';
 import { generate, Subscription } from 'rxjs';
+import * as e from 'express';
 
 @Component({
   selector: 'table-ranking',
@@ -27,6 +28,8 @@ export class TableRankingComponent {
     let players = [];
     let uniquePlayers = [];
     let gameData = data.weeksArray;
+    let playerData = [];
+    let playerWeeklyData = [];
 
     gameData.forEach(temp => {
       temp.players.forEach(temp => {
@@ -35,13 +38,60 @@ export class TableRankingComponent {
       })
     })
 
-    uniquePlayers.forEach(temp => {
-      this.tableRawData.push({
-        player: temp,
-        rating: 5
+    uniquePlayers.forEach(element => {
+      let data = {
+        name: element,
+        weeklyData: []
+      }
+      playerWeeklyData.push(data);
+    })
+
+    uniquePlayers.forEach(value => {
+      let data = {
+        player: value,
+        overallRating: []
+      }
+      gameData.forEach(temp => {
+        let week = temp.week;
+        let weekData = {
+          week: week,
+          rating: 0
+        }
+        let averageRating = [];
+        let averageWeekRating = [];
+        let weekRating: number;
+        let playerIsInWeek = false;
+
+        temp.players.filter(x => x.player === value)[0] ? playerIsInWeek = true : playerIsInWeek = false;
+
+        if (playerIsInWeek) {
+          const length = temp.players.filter(x => x.player === value)[0].rating?.length;
+          temp.players.filter(x => x.player === value)[0].rating.forEach(x => {
+            averageRating.push(x.rating);
+            let data1 = playerWeeklyData.find((element) => element.name === value);
+            let weekData = {
+              week: temp.week,
+              rating: x.rating
+            }
+
+            data1.weeklyData.push(weekData)
+
+            if (averageRating.length === length) {
+              weekRating = this.average(averageRating);
+              data.overallRating.push(weekRating);
+            };
+          });
+        }
+
       })
+      playerData.push(data);
+    })
+    playerData.map((x) => {
+      x.overallRating = this.average(x.overallRating);
     });
-    console.log(this.tableRawData);
+
+    this.optimizeDataForChart(playerWeeklyData);
+    this.tableRawData = playerData;
   }
 
   removeusingSet(arr) {
@@ -49,4 +99,37 @@ export class TableRankingComponent {
     return outputArray
   }
 
+  average(array) {
+    return array.reduce((a, b) => a + b) / array.length;
+  }
+
+  optimizeDataForChart(data: any) {
+
+    data.forEach(element => {
+      let data1 = element.weeklyData
+      const weeklyData = data1.reduce((a, b) => {
+        const found = a.find(e => e.week == b.week);
+        return found ? found.rating.push(b.rating) : a.push({ ...b, rating: [b.rating] }), a;
+      }, [])
+      weeklyData.map((element) => {
+        element.rating = this.average(element.rating);
+      })
+      // console.log(weeklyData);
+
+      element.weeklyData = weeklyData;
+    })
+
+    console.log(data);
+
+    // let data1 = data[0].weeklyData
+    // const res = data1.reduce((a, b) => {
+    //   const found = a.find(e => e.week == b.week);
+    //   return found ? found.rating.push(b.rating) : a.push({ ...b, rating: [b.rating] }), a;
+    // }, [])
+    // res.map((element) => {
+    //   element.rating = this.average(element.rating);
+    // })
+    // console.log(res);
+
+  }
 }
